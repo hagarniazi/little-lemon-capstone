5f02afd611189ccee946f2355f32ad7a38550108
Hagar <hagar.khaled.niazi@gmail.com>
2026-09-20 14:16:11 +0300

    Add semantic HTML and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,33 @@
+function Footer() {
+  return (
+    <footer>
+      <img src="/icons_assets/Logo.svg" alt="Little Lemon logo" />
+
+      <div>
+        <h2>Navigation</h2>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Contact</h2>
+        <p>Little Lemon Restaurant</p>
+        <p>Chicago, Illinois</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <img src="/icons_assets/Logo.svg" alt="Little Lemon logo" />
+    </header>
+  );
+}
+
+export default Header;
